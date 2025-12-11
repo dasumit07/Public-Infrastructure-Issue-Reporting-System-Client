b1bb4299b6387659ab77cc3fa97a7c562a4af502
dasumit7 <sd3034734@gmail.com>
2025-12-11 18:20:22 +0600

    accessToken send to server and verify user

--- src/Hooks/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect } from 'react';
+import UseAuth from './UseAuth';
+import { useNavigate } from 'react-router';
 
 const axiosSecure = axios.create({
     baseURL: "http://localhost:3000",
@@ -7,6 +9,32 @@ const axiosSecure = axios.create({
 });
 
 const UseAxiosSecure = () => {
+    const { user, logOutUser } = UseAuth();
+    const navigate = useNavigate();
+    console.log(user)
+    useEffect(() => {
+        const reqInterceptor = axiosSecure.interceptors.request.use(config => {
+            config.headers.Authorization = `Bearer ${user?.accessToken}`
+            return config
+        })
+
+        const resInterceptor = axiosSecure.interceptors.response.use((response) => {
+            return response;
+        },(err) => {
+            const statusCode = err.status;
+            if(statusCode === 401 || statusCode === 403){
+                logOutUser()
+                .then(() =>{
+                    navigate('/auth/login');
+                })
+            }
+            return Promise.reject(err);
+        })
+        return () => {
+            axiosSecure.interceptors.request.eject(reqInterceptor);
+            axiosSecure.interceptors.response.eject(resInterceptor);
+        }
+    }, [user, logOutUser, navigate])
     return axiosSecure;
 };
 
